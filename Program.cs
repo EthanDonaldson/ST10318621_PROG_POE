e8466b983b2867d061ac9f4428a1dc8044d1a162
Ethan James Donaldson <ethanali33@gmail.com>
2024-05-30 16:29:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10318621_PROG_POE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RecipeManager recipeManager = new RecipeManager();
+
+            while (true)
+            {
+                Console.WriteLine("1. Add Recipe");
+                Console.WriteLine("2. List Recipes");
+                Console.WriteLine("3. Exit");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        recipeManager.AddRecipe();
+                        break;
+                    case "2":
+                        recipeManager.ListRecipes();
+                        break;
+                    case "3":
+                        return;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Invalid choice.");
+                        Console.ResetColor();
+                        break;
+                }
+            }
+        }
+    }
+}
